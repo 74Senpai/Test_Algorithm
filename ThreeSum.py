0760bdd3ad905b68e36834d5b94d3fd3a8f12405
74_Senpai <ducthong7120042gmail.com>
2025-06-19 16:14:16 +0700

    ThreeSum Pass

--- ThreeSum.py
+++ ThreeSum.py
@@ -1,4 +1,6 @@
-def threeSum(self, nums: list[int]) -> list[list[int]]:
+class Ver1:
+    # FAILD : Time Limit Error
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         len_nums = len(nums)
         if len_nums <= 3:
             if nums[0] + nums[1] + nums[2] != 0:
@@ -28,3 +30,36 @@ def threeSum(self, nums: list[int]) -> list[list[int]]:
                 k_point = len_nums - 1
                 
         return threeSum
+    
+class Ver2:
+    # PASS : Time >=  666ms < 700ms 
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        len_nums = len(nums)
+        threeSum = []
+        pass_3Sum = {}
+        right = len_nums - 1
+        for i in range(len_nums - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  
+            left = i + 1
+            right = len_nums - 1
+            while True:
+                if left >= right:
+                    break
+                sum_ = nums[i] + nums[left] + nums[right]
+                if sum_ == 0:
+                    key = (nums[i], nums[left], nums[right])
+                    if key not in pass_3Sum:
+                        threeSum.append(list(key))
+                        pass_3Sum[key] = 1
+                    left += 1
+                    right -= 1  
+                elif sum_ < 0:
+                    left += 1  
+                else:
+                    right -= 1
+                
+        return threeSum
+
+print(Ver2().threeSum([-1,0,1,2,-1,-4]))
