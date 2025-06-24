a0f27b6d6341370b172fd2c16c2a1579695c9a34
74_Senpai <ducthong7120042gmail.com>
2025-06-24 12:49:52 +0700

    Update: down memory use by remove step init list and set

--- 2200FindAllKDis.py
+++ 2200FindAllKDis.py
@@ -1,18 +1,15 @@
 class Solution:
     def findKDistantIndices(self, nums: list[int], key: int, k: int) -> list[int]:
-        set_nums = set(nums)
-        if len(set_nums) == 1:
-            x = []
-            x += range(0,len(nums), 1)
-            return x
-            
+        if all(x == key for x in nums):
+            return list(range(len(nums)))
+        
         set_output = set()
         len_nums = len(nums)
         for i in range(len_nums):
             if nums[i] == key:
                 for x in range(max(i - k, 0), min(i + k + 1, len_nums), 1):
                     set_output.add(x)
-                    if x == len_nums:
-                        break
+                    if x == len_nums - 1:
+                        return list(set_output)
 
         return list(set_output)
