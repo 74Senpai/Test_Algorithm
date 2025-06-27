86ae27544e5b958283c17e1b156be2e9b41a2d81
74_Senpai <ducthong712004@gmail.com>
2025-06-27 16:11:26 +0700

    Add logic for remove excess number in matrix

--- 118_PascalTriangle.py
+++ 118_PascalTriangle.py
@@ -1,8 +1,10 @@
 def generate(self, numRows: int) -> list[list[int]]:
     triangle = [[1] * numRows for i in range(numRows)]
+    triangle[0] = triangle[0][0:1]
     for x in range(1, numRows):
         for y in range(1 , x):
             triangle[x][y] = triangle[x - 1][y - 1] + triangle[x - 1][y]
-
+        triangle[x] = triangle[x][0:x + 1]
+      
     return triangle
 print(generate(None, 5))
