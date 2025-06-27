3631978a9a9135720a87e1bbaa14fce504200dab
74_Senpai <ducthong712004@gmail.com>
2025-06-27 14:36:16 +0700

    Pascal Triangle: Update matrix befor( rotated 45 degrees ) => real matrix( Row by row )

--- 118_PascalTriangle.py
+++ 118_PascalTriangle.py
@@ -1,13 +1,8 @@
 def generate(self, numRows: int) -> list[list[int]]:
     triangle = [[1] * numRows for i in range(numRows)]
-    row = [[]] * numRows
-    row[0] = [1]
-    row[1] = [1,1]
     for x in range(1, numRows):
-        for y in range(1 , numRows - x):
-            triangle[x][y] = triangle[x][y - 1] + triangle[x - 1][y]
-            row[x].append(triangle[x][y])
+        for y in range(1 , x):
+            triangle[x][y] = triangle[x - 1][y - 1] + triangle[x - 1][y]
 
-    return row
-
+    return triangle
 print(generate(None, 5))
