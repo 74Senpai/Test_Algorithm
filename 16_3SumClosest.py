b432379835d9e69de971bb6cc1a6d02f3ee63d7a
74_Senpai <ducthong712004@gmail.com>
2025-07-08 22:37:20 +0700

    Three Sum closest pass, use Two point

--- 16_3SumClosest.py
+++ 16_3SumClosest.py
@@ -13,3 +13,23 @@ class Ver1:
         return closest
 
 print(Ver1().threeSumClosest([-1,2,1,-4], 1))
+
+class Ver2:
+    #PASS : 390ms, 18MB
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
+        nums.sort()
+        closest = nums[0] + nums[1] + nums[2]
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                cur = nums[left] + nums[right] + nums[i]
+                if cur < target:
+                    left += 1
+                else:
+                    right -= 1
+
+                if abs(cur - target) < abs(closest - target):
+                    closest = cur
+
+        return closest
