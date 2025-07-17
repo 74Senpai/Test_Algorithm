8ec9986c7907363cea634bea20a502ecb862b750
74_Senpai <ducthong712004@gmail.com>
2025-07-17 11:08:40 +0700

    Optimize performance

--- 3136_ValidWord.py
+++ 3136_ValidWord.py
@@ -1,4 +1,5 @@
-class Solution:
+class Ver1:
+    # 0 ms, 18MB
     def isValid(self, word: str) -> bool:
         vowels = {"a": "a", "e":"e", "i": "i", "o":"o", "u":"u"}
         ch_count = 0
@@ -17,3 +18,24 @@ class Solution:
         if ch_count >= 3 and  have_other_ch and have_vowels:
             return True
         return False
+    
+class Ver2:
+    #0ms, 17.60 MB
+    def isValid(self, word: str) -> bool:
+        if len(word) < 3:
+            return False
+
+        vowels = ("a", "e", "i", "o", "u")
+        contain_vowels = False
+        contain_other_ch = False
+        for i in word.lower():
+            if i in vowels:
+                contain_vowels = True
+                continue
+            
+            if i >= "b" and i <= "z":
+                contain_other_ch = True
+            elif i < "0" or i > "9":
+                return False
+
+        return contain_vowels and contain_other_ch
