31e29ab82281c52304dd72e5cbb0017e3fb4cea7
74_Senpai <ducthong712004@gmail.com>
2025-07-18 22:37:01 +0700

    Update Ver2: Master is master

--- 3201_FindMaxLengthSubsequence.py
+++ 3201_FindMaxLengthSubsequence.py
@@ -21,3 +21,21 @@ class Ver1:
                 dp_evenOdd.append(isEven)
         
         return max(even_count, len(nums) - even_count, len(dp_evenOdd), len(dp_oddEven))
+    
+
+class Ver2:
+    def maximumLength(self, nums: list[int]) -> int:
+        # Event == 0 , Odd == 1, c == case
+        if len(nums) == 2 :
+            return 2
+        
+        c00 = c01 = c11 = c10 = 0
+        for i in nums:
+            if i % 2 == 0:
+                c00 += 1
+                c10 = c01 + 1
+            else:
+                c11 += 1
+                c01 = c10 + 1
+        
+        return max(c00, c10, c11, c01)
